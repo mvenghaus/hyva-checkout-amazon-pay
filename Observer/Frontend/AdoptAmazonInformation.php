161e940397be3fbbb83f8e215034a3af63e801a1
Marcus Venghaus <marcus.venghaus@inklammern.de>
2023-03-03 13:36:27 +0100

    -

--- Observer/Frontend/AdoptAmazonInformation.php
+++ Observer/Frontend/AdoptAmazonInformation.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace MVenghaus\HyvaCheckoutAmazonPay\Observer\Frontend;
 
 use Amazon\Pay\Api\CheckoutSessionManagementInterface as AmazonCheckoutSessionManagementInterface;
+use Amazon\Pay\Gateway\Config\Config;
 use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 
-class SetShippingAddressFromAmazon implements ObserverInterface
+class AdoptAmazonInformation implements ObserverInterface
 {
     public function __construct(
         private readonly RequestInterface $request,
@@ -21,19 +22,26 @@ class SetShippingAddressFromAmazon implements ObserverInterface
 
     public function execute(Observer $observer)
     {
+        $quote = $this->checkoutSession->getQuote();
+
         $amazonCheckoutSessionId = $this->request->getParam('amazonCheckoutSessionId');
         if (!empty($amazonCheckoutSessionId)) {
             $this->checkoutSession->setAmazonCheckoutSessionId($amazonCheckoutSessionId);
 
-            $quote = $this->checkoutSession->getQuote();
+
             $amazonShippingAddress = $this->amazonCheckoutSessionManagement->getShippingAddress($amazonCheckoutSessionId);
             $quote->getShippingAddress()->addData(current($amazonShippingAddress));
             $quote->getBillingAddress()->addData(current($amazonShippingAddress));
+            $quote
+                ->getPayment()
+                ->setQuote($quote)
+                ->setMethod(Config::CODE)
+                ->getMethodInstance();
+
             $quote->save()->collectTotals();
 
             header('Location: /checkout/');
             exit;
         }
-
     }
 }
